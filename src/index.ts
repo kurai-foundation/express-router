e1786326da9b49680de99f3d1ca5169ecef96f81
knownout <a.slavinskii@icloud.com>
2025-05-11 07:44:48 +0300

    Add FileResponse and Redirect into default exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Application from "./application"
 import { ParsedQs, RequestMethods, RouteMetadata } from "./requests/router-requests-core"
 import CustomResponse, { ResponseFactory } from "./responses/custom-response"
+import FileResponse from "./responses/file-response"
+import Redirect from "./responses/redirect"
 import { combineMiddlewares } from "./utils/middleware/combine-middlewares"
 import { Middleware } from "./utils/middleware/middleware"
 import RouterBuilder from "./utils/routing/router-builder"
@@ -12,6 +14,8 @@ export {
   ResponseFactory,
   CustomResponse,
   RouterBuilder,
+  FileResponse,
+  Redirect,
   routerUtils,
   Application,
   type ISchema,
